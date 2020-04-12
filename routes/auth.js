59c5d48d1b6d8d12e94538fa4c8967fcb9774f58
Severin Spoerri <ssp@gutknecht-informatik.com>
2020-04-12 23:24:10 +0200

    added auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
 
 // @route   GET api/auth
 // @desc    Get logged in user
@@ -13,8 +18,54 @@ router.get("/", (req, res) => {
 // @desc    Auth user & get token
 // @access  Public
 
-router.post("/", (req, res) => {
-  res.send("Login User");
-});
+router.post(
+  "/",
+  [
+    check("email", "please include a valid email").isEmail(),
+    check("password", "password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "User not found" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "wrong password" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
