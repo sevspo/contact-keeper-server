7e373afa6d0830bfc0fcbb723ea305ef7d6d6d24
Severin Spoerri <ssp@gutknecht-informatik.com>
2020-05-01 21:00:54 +0200

    auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,12 @@ const auth = require('../middleware/auth');
 
 router.get('/', auth, async (req, res) => {
   try {
-  } catch (error) {}
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error');
+  }
 });
 
 // @route   POST api/auth
